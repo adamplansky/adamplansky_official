8075fc0bfa63070983e78ec7f11ad87a6fd55cc0
Adam Plansky <adamplansky@gmail.com>
2015-03-01 15:48:17 +0100

    Lucie countdown

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,13 @@ class BlogsController < ApplicationController
   def index
     
   end
+  
+  def count_down
+    @future = Date.new(2015,9,6)
+    @past = Date.new(2014,10,17)
+    @now = Date.today
+    @overall = (@future - @past)
+    @zamnou = (@now - @past).to_i
+    @procenta = ((@zamnou.to_f / @overall)*100).round(2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'blogs#index'
+  match "countdown", :to => "blogs#count_down", :via => :get  
 end
