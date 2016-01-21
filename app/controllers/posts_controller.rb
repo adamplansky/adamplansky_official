6aa08e5b196b090c578f52161aee16de4233bec8
Adam Plansky <adamplansky@gmail.com>
2016-01-21 15:03:18 +0100

    blog markdown alpha

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order("published_at DESC")
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.published_at = Time.now
+    @post.save
+    redirect_to @post
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
