ef335dcde60f3348c1cd4cca1550dcd6ccc7d6de
Adam Plansky <adamplansky@gmail.com>
2016-10-20 09:56:22 +0200

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'weights/index'
+#  get 'weights/index'
 
-
+  resources :weights,only: [:index]
   resources :posts,only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   root to: 'blogs#index'
   match "countdown", :to => "blogs#count_down", :via => :get
-  # MarkdownExample::Application.routes.draw do
-  get 'weights/index'
 
   devise_for :users
   #   resources :posts, only: [:index, :show]
